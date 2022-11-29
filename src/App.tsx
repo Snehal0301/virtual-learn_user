271a0a6cb1fc6e205823959df800e78ab9b78317
Snehal0301 <snehal@robosoftin.com>
2022-11-29 16:49:00 +0530

    Changed branch to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { facebook,google } from './svg'
 const App = () => {
   return (
-    <div className='app'>App</div>
+    <div className='app'>
+      {google}
+    </div>
   )
 }
 
