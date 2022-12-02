109c44bfa69350ad256424ded9319ef6adca14e2
Snehal0301 <snehal@robosoftin.com>
2022-12-02 09:14:00 +0530

    Bug Updated by Mamatha in dev

--- src/components/onboarding/registration/registrationform/schema/MobileSchema.tsx
+++ src/components/onboarding/registration/registrationform/schema/MobileSchema.tsx
@@ -1,6 +1,6 @@
 import * as Yup from "yup"
 import 'yup-phone'
 export const mobilenumberSchema = Yup.object({
-   mobilenumber: Yup.string().phone()
+   Mobilenumber: Yup.string().phone()
     .required()
 });
