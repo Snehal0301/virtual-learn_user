b7c4753c530127784923dc05e2b44afce094dde0
PallaviKini7 <pallavi.kini@robosoftin.com>
2022-12-03 17:00:30 +0530

    Edited the start page by pallavi

--- src/components/homePage/start/start.tsx
+++ src/components/homePage/start/start.tsx
@@ -1,20 +1,204 @@
-import './Start.css';
 import React, { useState } from 'react';
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import './Start.css';
+import { design, start_pauseIcon, start_timeIcon } from '../../../utils/svgIcons';
 const Start = () => {
 
+  const startCourseData = [
+    {
+
+      id: 1,
+      image: require('../../../assets/images/start-courses-image/What-Are-The-Most-Effective-Digital-Marketing-Strategies-_blog 1.png'),
+      title: "Digital Marketing",
+      chapter: '2 chapter',
+      time: "2:23:24",
+      btntext :"Design"
+    },
+    {
+      id: 2,
+      image: require('../../../assets/images/start-courses-image/banner-42 1.png'),
+      title: "Marketing Courses",
+      chapter: '2 chapter',
+      time: "2:23:24",
+      btntext :"Marketing"
+    },
+    {
+      id: 3,
+      image: require('../../../assets/images/start-courses-image/What-Are-The-Most-Effective-Digital-Marketing-Strategies-_blog 1.png'),
+      title: "Digital Marketing Strategies",
+      chapter: '2 chapter',
+      time: "2:23:24",
+      btntext :"Design"
+    },
+
+  ]
+
+  const startCategories = [
+    'Design',
+    'Development',
+    'Business',
+    'Music',
+    'Finance',
+    'Health & Fitness',
+    'IT & Software',
+    'Marketing',
+    'Lifestyle',
+    'Photography',
+
+  ];
   return (
     <div className="start">
       <div className="start-greeting">Hello!</div>
       <div className="start-username">Mahendra Singh Dhoni</div>
-      <div className="start-slideShow">SlideShow</div>
-      <div className="start-ongoingCourses">Ongoing Courses</div>
-      <div className="start-categories">Categories</div>
-      <div className="start-courseChoice">Course choice</div>
-      <div className="start-courseChoice">Top courses in business</div>
-      <div className="start-courseChoice">Top courses in design</div>
-      <button onClick={() => alert("Clicked")}>Click me</button>
-    </div >
+      <div className='start-card'>
+        <div className='start-course1'>
+          {
+            startCourseData.map(item =>
+            (
+              <div className='start-course1-image'>
+                <img src={item.image} alt="" />
+                <div className='start-course-title'>{item.title}</div>
+              </div>
+
+            ))
+          }
+        </div>
+      </div>
+      <div className='start-course-section2'>
+        <div className='start-ongoing-courses'>Ongoing courses</div>
+        <div className='start-seeall'>See All</div>
+      </div>
+      <div className='start-card2'>
+        <div className='start-course1'>
+          {
+            startCourseData.map(item =>
+            (
+              <div className='start-course1-image'>
+                <img src={item.image} alt="" />
+                <div className='start-title-container'>
+                  <div className='start-title-chapter'>
+                    <div className='start-course-section2-title'>{item.title}</div>
+                    <div className='start-course-chapter'>{item.chapter}</div>
+                  </div>
+
+                  <button className='start-course-button'>Continue</button>
+
+                </div>
+              </div>
+
+
+            ))
+          }
+        </div>
+      </div>
+      <div className='start-course-section2'>
+        <div className='start-ongoing-courses'>Categories</div>
+        <div className='start-seeall'>See All</div>
+      </div>
+
+      <div className="start-course-categories">
+
+        <div className="start-course-categories-Body">
+          {startCategories.map((ele: any, i: any) => {
+            return (
+              <div
+                className="start-course-categories-Parent"
+                key={i}
+              >
+                <div className="start-course-categories-Icon">
+                  {design}
+                </div>
+                <div className="start-course-categories-Name">
+                  {ele}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className='start-course-section2'>
+        <div className='start-ongoing-courses'>Choice your course</div>
+        <div className='start-seeall'>See All</div>
+      </div>
+      <div className='start-choice-course-subcategory'>
+        <div className='start-subcategory-all'>All</div>
+        <div className='start-subcategory-all'>Popular</div>
+        <div className='start-subcategory-all'>Newest</div>
+      </div>
+      <div className='start-card'>
+        <div className='start-choice1'>
+          {
+            startCourseData.map(item =>
+            (
+              <div className='start-choice-subcategory-image'>
+                
+                <div className='start-image-pause'>
+                  <img src={item.image} alt="" />
+                  <button className='start-designbtn'>{item.btntext}</button>
+                </div>
+                <div className='start-choice-subcategory-title'>{item.title}</div>
+                <div className='start-choice-chapter'>{item.chapter}</div>
+              </div>
+
+            ))
+          }
+        </div>
+      </div>
+      <div className='start-course-section2'>
+        <div className='start-ongoing-courses'>Top courses in Business</div>
+        <div className='start-seeall'>See All</div>
+      </div>
+      <div className='start-card'>
+        <div className='start-choice1'>
+          {
+            startCourseData.map(item =>
+            (
+              <div className='start-choice-subcategory-image'>
+                <div className='start-image-pause'>
+                  <img src={item.image} alt="" />
+                  <div className='start-pauseIcon'>{start_pauseIcon}</div>
+                </div>
+                <div className='start-choice-subcategory-title'>{item.title}</div>
+                <div className='start-chapter-time'>
+                  <div className='start-choice-chapter2'>{item.chapter}</div>
+                  <div>{start_timeIcon}</div>{item.time}
+                </div>
+              </div>
+
+            ))
+          }
+        </div>
+      </div>
+      <div className='start-course-section2'>
+        <div className='start-ongoing-courses'>Top courses in Design</div>
+        <div className='start-seeall'>See All</div>
+      </div>
+      <div className='start-card'>
+        <div className='start-choice1'>
+          {
+            startCourseData.map(item =>
+            (
+              <div className='start-choice-subcategory-image'>
+                <div className='start-image-pause'>
+                  <img src={item.image} alt="" />
+                  <div className='start-pauseIcon'>{start_pauseIcon}</div>
+                </div>
+                <div className='start-choice-subcategory-title'>{item.title}</div>
+                <div className='start-chapter-time'>
+                  <div className='start-choice-chapter2'>{item.chapter}</div>
+                  <div>{start_timeIcon}</div>{item.time}
+                </div>
+              </div>
+
+            ))
+          }
+        </div>
+      </div>
+    </div>
+
+
   );
 };
 
