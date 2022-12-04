d0dadec490253bc968fe76054317eb2b8f32bd1e
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-12-04 12:32:34 +0530

    quiz-header head completed -hemraj

--- src/components/homePage/quiz/quizResults/QuizResults.tsx
+++ src/components/homePage/quiz/quizResults/QuizResults.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import './QuizResults.css';
 
 const QuizResults = () => {
+  const results: any = [
+    { question: 'Question1', answer: 'Correct Answer' },
+    { question: 'Question2', answer: 'Correct Answer' },
+    { question: 'Question3', answer: 'Wrong Answer' },
+    { question: 'Question4', answer: 'Correct Answer' },
+    { question: 'Question5', answer: 'Wrong Answer' },
+    { question: 'Question6', answer: 'Wrong Answer' },
+    { question: 'Question7', answer: 'Wrong Answer' },
+    { question: 'Question8', answer: 'Correct Answer' },
+    { question: 'Question9', answer: 'Correct Answer' },
+    { question: 'Question10', answer: 'Correct Answer' },
+  ];
+
   return (
     <div className="quizResults">
       <div className="quizResults-header">
@@ -44,7 +57,26 @@ const QuizResults = () => {
           </div>
         </div>
       </div>
-      <div className="quizResults-body">Body</div>
+      <div className="quizResults-body">
+        <div className="quizResults-bodyListTitle">List of Questions</div>
+        <div className="quizResults-bodyList">
+          {results.map((ele: any, i: any) => {
+            return (
+              <div className="quizResults-bodyListItem" key={i}>
+                <div className="quizResults-bodyListItemContent">
+                  <div className="quizResults-bodyListItemContentQuestion">
+                    {ele.question}
+                  </div>
+                  <div className="quizResults-bodyListItemContentAnswer">
+                    {ele.answer}
+                  </div>
+                </div>
+                <div className="quizResults-bodyListItemDrawer">{'>'}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
