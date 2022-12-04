b1dc4d92eb2e26d41d008092943cd311feef1898
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-12-04 09:17:19 +0530

    quiz-successPage

---
+++ src/components/homePage/quiz/quizSuccessPage/quizSuccess.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../../../../views/onboarding/success_page/Success_Page.css';
+
+const QuizSuccess = () => {
+  return (
+    <div className="succesContainer">
+      <div className="successContent">
+        <div className="successImage">
+          <img
+            src={require('../../../../assets/images/img_moduletest_success_illustration 1.png')}
+            alt="module test success"
+          />
+        </div>
+        <div className="successText">Congratulations!</div>
+        <div className="successDesc">
+          You have completed{' '}
+          <strong style={{ fontWeight: '900' }}>
+            Chapter 3 - Setting up a new project
+          </strong>{' '}
+          from Course: Learn Figma - UI/UX Design Essential Training
+        </div>
+        <div className="getStarted" style={{ cursor: 'pointer' }}>
+          Result
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuizSuccess;
--- src/views/dashboard/HomePage.tsx
+++ src/views/dashboard/HomePage.tsx
@@ -1,5 +1,7 @@
 import Header from '../../components/homePage/header/Header';
 import Quiz from '../../components/homePage/quiz/Quiz';
+import QuizSuccess from '../../components/homePage/quiz/quizSuccessPage/quizSuccess';
+
 import Start from '../../components/homePage/start/start';
 import './HomePage.css';
 
@@ -12,7 +14,8 @@ const HomePage = () => {
 
       <div className="homePageBody">
         {/* <Start /> */}
-        <Quiz />
+        {/* <Quiz /> */}
+        <QuizSuccess />
       </div>
     </div>
   );
