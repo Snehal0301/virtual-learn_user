a1e92aae57718237cb868bbda1aa685bef862744
Snehal0301 <snehal@robosoftin.com>
2022-12-05 10:38:30 +0530

    Changed from mycourse-flow mamatha

--- src/components/homePage/mycourse/ongoing/Ongoing.jsx
+++ src/components/homePage/mycourse/ongoing/Ongoing.jsx
@@ -8,31 +8,56 @@ const Ongoing = () => {
         title: "Art & Illustration",
         chapters: "15/20 Chapters",
       },
+      {
+        image: require("../../../../assets/images/Servant-Leadership 1.png"),
+        title: "Art & Illustration",
+        chapters: "15/20 Chapters",
+      }, {
+        image: require("../../../../assets/images/Servant-Leadership 1.png"),
+        title: "Art & Illustration",
+        chapters: "15/20 Chapters",
+      }, {
+        image: require("../../../../assets/images/Servant-Leadership 1.png"),
+        title: "Art & Illustration",
+        chapters: "15/20 Chapters",
+      },
+      {
+        image: require("../../../../assets/images/Servant-Leadership 1.png"),
+        title: "Art & Illustration",
+        chapters: "15/20 Chapters",
+      },
     ];
-    return (
-        ongoingCourses.map((ele,i) => {
-            return (
-              <div className="ongoing-parent" key={i}>
-                <div className="ongoing-images">
-                  <img
-                    src={ele.image}
-                    alt=""
-                    className="ong-img"
-                  />
-                  <div className="chap-progress">
-                    <p className="Ongoing-text">Ongoing</p>
-                    <div className="chap-descp">
-                                <p>{ele.title}</p>
-                                <p>{ele.chapters}</p>
-                    </div>
-                  </div>
-                  <button className="btn-continue-ongoing">Continue</button>
-                </div>
-              </div>
-            );
-      })
-    
+  return (
+      <div className="ongoing-section">
+     { ongoingCourses.map((ele,i)=>{
+        return(
+      <div className="ongoing-parent" key={i}>
+            <div className="ongoing-images">
+              <div className="ong-overlay"></div>
+          <img
+            src={ele.image}
+            alt=""
+            className="ong-img"
+          />
+          <div className="chap-progress">
+            <p className="Ongoing-text">Ongoing</p>
+            <div className="chap-descp">
+              <p>{ele.title}</p>
+              <p>{ele.chapters}</p>
+            </div>
+          </div>
+          <button className="btn-continue-ongoing">Continue</button>
+        </div>
+      </div>
+        )
+     })
+      }
+    </div>
   );
 };
 
 export default Ongoing;
+
+/*
+
+ */
