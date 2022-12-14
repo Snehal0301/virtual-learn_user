d33c719bd0186fc0a06f50833d602a9e8e4855e6
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-12-14 20:54:12 +0530

    my course empty

--- src/components/homePage/mycourse/EmptyCourse.tsx
+++ src/components/homePage/mycourse/EmptyCourse.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { advancedCourse } from '../../../redux/reducers/advancedCourse';
+import {
+  basicCourse,
+  categoryName,
+} from '../../../redux/reducers/basicCourses';
+import { subCategories } from '../../../redux/reducers/subCategories';
 import { design } from '../../../utils/svgIcons';
 
 const EmptyCourse = () => {
-  const Categories = [
-    'Design',
-    'Development',
-    'Business',
-    'Finance',
-    'Health & Fitness',
-    'Music',
-    'IT & Software',
-    'Marketing',
-    'Lifestyle',
-    'Photography',
-  ];
+  const [Categories, setcategories] = useState([]);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://virtuallearn-env.eba-6xmym3vf.ap-south-1.elasticbeanstalk.com/user/categoriesWP`,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('Token')}`,
+          },
+        }
+      )
+      .then((res) => {
+        setcategories(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div className="mycourse-body">
       <div className="mycourse-emptyImg-section">
@@ -35,14 +54,34 @@ const EmptyCourse = () => {
           <p>Categories</p>
         </div>
         <div className="categories-eachCategories">
-          {Categories.map((ele, i) => {
+          {Categories.map((ele: any, i) => {
             return (
               <div
                 className="categoriesDisplay-parent categories-chipBorder"
                 key={i}
+                onClick={() => {
+                  dispatch(categoryName(ele.categoryName));
+                  dispatch(
+                    basicCourse(`basicCourses?categoryId=${ele.categoryId}`)
+                  );
+                  dispatch(
+                    advancedCourse(
+                      `advanceCourses?categoryId=${ele.categoryId}`
+                    )
+                  );
+                  dispatch(
+                    subCategories(`subCategories?categoryId=${ele.categoryId}`)
+                  );
+                  navigate('/categories/design');
+                }}
               >
-                <div className="categoriesDisplay-Icons">{design}</div>
-                <div className="categoriesDisplay-Names">{ele}</div>
+                <div className="categoriesDisplay-Icons">
+                  {' '}
+                  <img src={ele.categoryPhoto} alt="" />
+                </div>
+                <div className="categoriesDisplay-Names">
+                  {ele.categoryName}
+                </div>
               </div>
             );
           })}
