88e535ca043cc398d0ecab035d2b6eba79c836bc
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-12-15 06:08:49 +0530

    prevent onboarding screen if already logged in

--- src/components/onboarding/login/login-verification/login-otp/LoginOtp.jsx
+++ src/components/onboarding/login/login-verification/login-otp/LoginOtp.jsx
@@ -1,74 +1,74 @@
-import "./LoginOtp.css";
-import OTPInput, { ResendOTP } from "otp-input-react";
-import React, { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import './LoginOtp.css';
+import OTPInput, { ResendOTP } from 'otp-input-react';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   changePassword,
   registerOtp,
   registerPersonalDetails,
-} from "../../../../../redux/reducers/Conditions";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+} from '../../../../../redux/reducers/Conditions';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LoginVerfication = () => {
-  const [OTP, setOTP] = useState("");
+  const [OTP, setOTP] = useState('');
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const location = useLocation();
 
-  console.log("location", location.pathname);
+  console.log('location', location.pathname);
 
   const changePass = useSelector((state) => state.loginConditions.passChange);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if (location.pathname === "/onboarding/registerOtp") {
+    if (location.pathname === '/onboarding/registerOtp') {
       if (OTP.length === 4) {
-        sendOtpServer(OTP, "/newUser/verify");
+        sendOtpServer(OTP, '/newUser/verify');
       }
       // dispatch(registerPersonalDetails(true));
       // navigate("/onboarding/personalDetails");
-    } else if (location.pathname === "/onboarding/otpVerification") {
+    } else if (location.pathname === '/onboarding/otpVerification') {
       // dispatch(changePassword(true));
       // navigate("/onboarding/changePassword");
-      sendOtpServer(OTP, "/newUser/verify");
+      sendOtpServer(OTP, '/newUser/verify');
     }
   };
 
   const sendOtpServer = (otp, url) => {
-    console.log("OTP", otp, url);
+    console.log('OTP', otp, url);
     fetch(
       `http://virtuallearn-env.eba-6xmym3vf.ap-south-1.elasticbeanstalk.com${url}`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          mobileNumber: localStorage.getItem("regMobileNum"),
+          mobileNumber: sessionStorage.getItem('regMobileNum'),
           oneTimePassword: otp,
         }),
       }
     )
       .then((res) => res.json())
       .then((res) => {
-        console.log("response", res);
-        if (location.pathname === "/onboarding/registerOtp") {
-          if (res && res.message === "Verified") {
+        console.log('response', res);
+        if (location.pathname === '/onboarding/registerOtp') {
+          if (res && res.message === 'Verified') {
             dispatch(registerPersonalDetails(true));
-            navigate("/onboarding/personalDetails");
-          } else if (res.message === "Input field is incorrect") {
+            navigate('/onboarding/personalDetails');
+          } else if (res.message === 'Input field is incorrect') {
             dispatch(registerPersonalDetails(false));
             showError(res.message);
           }
         }
-        if (location.pathname === "/onboarding/otpVerification") {
-          if (res && res.message === "Verified") {
+        if (location.pathname === '/onboarding/otpVerification') {
+          if (res && res.message === 'Verified') {
             dispatch(changePassword(true));
-            navigate("/onboarding/changePassword");
+            navigate('/onboarding/changePassword');
           } else {
             dispatch(registerPersonalDetails(false));
             showError(res.message);
@@ -78,7 +78,7 @@ const LoginVerfication = () => {
   };
 
   useEffect(() => {
-    console.log("change pass", changePass);
+    console.log('change pass', changePass);
   }, [changePass]); //when otp is valid(this comes from redux)
 
   const showError = (msg) => {
@@ -86,14 +86,14 @@ const LoginVerfication = () => {
       <div className="loginAuth-showError">
         <div className="loginAuth-showErrorIcon">
           <img
-            src={require("../../../../../assets/icons/icn_invalid error.png")}
+            src={require('../../../../../assets/icons/icn_invalid error.png')}
             alt="invalid"
           />
         </div>
         <div className="loginAuth-showErrorMessage">{msg}</div>
       </div>,
       {
-        position: "bottom-right",
+        position: 'bottom-right',
         autoClose: 5000,
         hideProgressBar: true,
         pauseOnHover: true,
@@ -119,16 +119,16 @@ const LoginVerfication = () => {
             otpType="number"
             disabled={false}
             inputStyles={{
-              width: "30%",
-              outline: "none",
-              fontSize: "20px",
-              paddingBottom: "15px",
-              backgroundColor: "transparent",
-              color: "white",
-              borderTop: "none",
-              borderLeft: "none",
-              borderRight: "none",
-              borderBottom: "1px solid rgba(255, 255, 255, 0.411)",
+              width: '30%',
+              outline: 'none',
+              fontSize: '20px',
+              paddingBottom: '15px',
+              backgroundColor: 'transparent',
+              color: 'white',
+              borderTop: 'none',
+              borderLeft: 'none',
+              borderRight: 'none',
+              borderBottom: '1px solid rgba(255, 255, 255, 0.411)',
             }}
           />
           {/* <ResendOTP onResendClick={() => console.log("Resend clicked")} /> */}
