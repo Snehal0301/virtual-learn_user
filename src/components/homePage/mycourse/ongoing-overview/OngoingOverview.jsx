bad30ca5a06d019d37cd8e6c9d2c90f26b324823
Snehal0301 <snehal@robosoftin.com>
2022-12-19 22:26:36 +0530

    Added certificate page

--- src/components/homePage/mycourse/ongoing-overview/OngoingOverview.jsx
+++ src/components/homePage/mycourse/ongoing-overview/OngoingOverview.jsx
@@ -66,6 +66,7 @@ const OngoingOverview = () => {
   const [chapterLoading, setChapterLoading] = useState(false);
   const [overviewLoading, setOverviewLoading] = useState(false);
   const [testLoading, setTestLoading] = useState(false);
+  const [videoPlayState, setVideoPlayState] = useState(true);
   const dispatch = useDispatch();
 
   // Toast
@@ -278,8 +279,6 @@ const OngoingOverview = () => {
         console.log(res);
         // dispatch(courseOverview(pauseData.courseId));
         dispatch(chapterResponse(pauseData.courseId));
-        // if (res.data.message === "Updated SuccessFully") {
-        // }
       })
       .catch((err) => {
         console.log(err);
@@ -304,34 +303,6 @@ const OngoingOverview = () => {
 
     (unmountData.chapterId !== '') && (unmountData.pauseTime !== '') && (unmountData.lessonId !== '') && (unmountData.courseId !== '') &&
       dispatch(pauseUnmount(unmountData));
-
-    // await axios
-    //   .request(
-    //     `http://virtuallearn-env.eba-6xmym3vf.ap-south-1.elasticbeanstalk.com/user/pauseTime`,
-    //     {
-    //       method: 'put',
-    //       headers: {
-    //         Authorization: `Bearer ${sessionStorage.getItem('Token')}`,
-    //       },
-    //       data: {
-    //         pauseTime: unmountPauseTime,
-    //         lessonId: pauseData.lessonId,
-    //         chapterId: pauseData.chapterId,
-    //         courseId: pauseData.courseId
-    //       },
-    //     }
-    //   )
-    //   .then((res) => {
-    //     console.log(res)
-    //     console.log('unmountPauseTime', unmountPauseTime)
-    //     // dispatch(courseOverview(pauseData.courseId));
-    //     // dispatch(chapterResponse(pauseData.courseId));
-    //     // if (res.data.message === "Updated SuccessFully") {
-    //     // }
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
   };
 
   const continueModalData = async () => {
@@ -348,6 +319,8 @@ const OngoingOverview = () => {
       .then((res) => {
         console.log('modal continue data', res)
         setContinueModal(res.data)
+        // dispatch(accordianIDState(continueModal.chapterNumber))
+        // accordianToggle(continueModal.chapterNumber-1)
       })
       .catch((err) => {
         console.log('Modal continue error', err);
@@ -357,24 +330,39 @@ const OngoingOverview = () => {
   useEffect(() => {
     chapter &&
       continueModalData()
+
+    setPauseData({
+      videoTitle: continueModal.lessonName
+    })
+    // accordianToggle(accordianStateID - 1);
+    // accordianToggle(continueModal.chapterNumber - 1)
+    console.log('pauseData.videoTitle', pauseData.videoTitle);
   }, [chapter])
+
 
   useEffect(() => {
     console.log('Component mounted');
-    // dispatch(accordianIDState(accordianStateID-1))
-    accordianToggle(accordianStateID - 1);
 
+    // look here
+    accordianToggle(accordianStateID - 1);
     return () => {
       console.log('Component unmounted');
       dispatch(tabToggleState(1));
       dispatch(unmountState('true'));
       dispatch(accordianToggleState(0));
+      // dispatch(accordianIDState(0))
       setDefaultVideo('');
       // dispatch(firstVideoState('https://youtu.be/d1UNXbRxxZE'));
       dispatch(videoLinkState(''));
       componentUnMount();
     };
   }, []);
+
+  // useEffect(() => {
+
+  //   dispatch(accordianIDState(continueModal.chapterNumber))
+  //   accordianToggle(accordianStateID - 1);
+  // },[])
 
   // componentUnMount()
   const unmountPauseTime = new Date(pauseStateID * 1000)
@@ -464,7 +452,6 @@ const OngoingOverview = () => {
   }, [joinCourse]);
 
   const [accState, setAccState] = useState(0);
-  const [loop, setLoop] = useState(false);
   const [nextModal, setNextModal] = useState(false);
   const [defPause, setDefPause] = useState(false);
   const [firstPause, setFirstPause] = useState(true);
@@ -473,14 +460,12 @@ const OngoingOverview = () => {
   const playerRef = useRef();
 
   const defaultNormalPause = (time) => {
-    var hms = time;   // your input string
-    var a = hms.split(':'); // split it at the colons
-
-    // minutes are worth 60 seconds. Hours are worth 60 minutes.
+    var hms = time;
+    var a = hms.split(':');
     var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
 
     console.log('seconds', seconds);
-    
+
     playerRef.current.seekTo(seconds, 'seconds');
 
     setPause(false);
@@ -489,6 +474,8 @@ const OngoingOverview = () => {
     setNextModal(false);
     setFirstPause(false);
     showChapter(chapter.courseId, continueModal.chapterId, continueModal.lessonId, continueModal.lessonName)
+    setVideoPlayState(false)
+    // setVtitle(true)
     dispatch(unmountState('false'));
   };
 
@@ -588,62 +575,68 @@ const OngoingOverview = () => {
               </>
             )}
 
-            {unmountStateRedux === 'true' &&
+            {continueModal &&
               chapter &&
               chapter.enrolled === true && (
                 <>
-                  <div className="pause-overlay">
-                    {firstPause && (
-                      <div
-                        className="continue-chapter-pause-button"
-                        onClick={() => {
-                          setNextModal(true);
-                          setFirstPause(false);
-                        }}
-                      >
-                        Continue Chapter {continueModal.chapterNumber} Lesson{' '}
-                        {continueModal.lessonNumber}
-                      </div>
-                    )}
-
-                    {nextModal && (
-                      <div className="onpause-modal">
-                        <p className="onpause-modal-title">
-                          Your lesson paused at{' '}
-                          <span>{continueModal.pauseTime}</span> Do you want to
-                          continue watching?
-                        </p>
-                        {/* <div className="onpause-modal-button"> */}
-                        <button
-                          className="onpause-button"
-                          onClick={()=>defaultNormalPause(continueModal.pauseTime)}
-                        >
-                          Continue Watching
-                        </button>
-                        <button
-                          className="onpause-button beginning"
+                  {
+                    videoPlayState &&
+                    <div div className="pause-overlay">
+                      {firstPause && (
+                        <div
+                          className="continue-chapter-pause-button"
                           onClick={() => {
-                            playerRef.current.seekTo(0, 'seconds');
-                            setPause(false);
-                            setPlaying(true);
-                            dispatch(unmountState('false'));
+                            setNextModal(true);
+                            setFirstPause(false);
                           }}
                         >
-                          Watch from beginning
-                        </button>
-                      </div>
-                    )}
-                    {defPause && (
+                          Continue Chapter {continueModal.chapterNumber} Lesson{' '}
+                          {continueModal.lessonNumber}
+                        </div>
+                      )}
+
+                      {nextModal && (
+                        <div className="onpause-modal">
+                          <p className="onpause-modal-title">
+                            Your lesson paused at{' '}
+                            <span>{continueModal.pauseTime}</span> Do you want to
+                            continue watching?
+                          </p>
+                          <button
+                            className="onpause-button"
+                            onClick={() => defaultNormalPause(continueModal.pauseTime)}
+                          >
+                            Continue Watching
+                          </button>
+                          <button
+                            className="onpause-button beginning"
+                            onClick={() => {
+                              playerRef.current.seekTo(0, 'seconds');
+                              setPause(false);
+                              setPlaying(true);
+                              dispatch(unmountState('false'));
+                            }}
+                          >
+                            Watch from beginning
+                          </button>
+                        </div>
+                      )}
+                      {/* {defPause && (
                       <div className="pause-button" onClick={onPlay}>
                         {start_pauseIconVideo}
                       </div>
-                    )}
-                    <div className="video-title-overlay">
-                      {pauseData.videoTitle}
+                    )} */}
+                      {
+                        vtitle &&
+                        <div className="video-title-overlay">
+                          {pauseData.videoTitle}
+                        </div>
+                      }
                     </div>
-                  </div>
+                  }
                 </>
-              )}
+              )
+            }
 
             <ReactPlayer
               url={videoLink ? videoLink : defaultvideo}
@@ -699,75 +692,86 @@ const OngoingOverview = () => {
           ) : (
             <h3>Loading</h3>
           )}
-          {tabState === 1 ? (
-            <>
-              {overviewData ? (
-                <div className="ongoing-course-desc">
-                  <div className="ongoing-course-desc-title">
-                    <p>{overviewData.courseTagLine}</p>
-                  </div>
-                  <div className="ongoing-course-desc-content">
-                    <input type="checkbox" id="expanded"></input>
-                    <ShowMoreText
-                      className="showmore"
-                      anchorClass="show-more-style"
-                    >
-                      {overviewData.description}
-                    </ShowMoreText>
-                    {/* <ShowMoreText className="showmore" anchorClass="show-more-style">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis quod ullam, iste quo hic voluptatem, et tempore, modi quaerat placeat cum. Quam eos itaque quo omnis, quae delectus illo quos autem nobis ut possimus in, excepturi et illum sunt perspiciatis accusamus, repellat facilis fuga animi voluptatem pariatur nisi consequatur voluptates! Rem cum nobis itaque consequuntur eveniet a nihil ab quibusdam deleniti dolorum quia libero ullam culpa minima eaque debitis minus aperiam ducimus odio delectus, eius consequatur iusto odit! Nemo quasi corporis velit itaque neque fugit soluta dolores dolorem facere, molestias maxime non consequatur quidem odio totam esse iure, delectus fuga!</ShowMoreText> */}
-                    {/* <label for="expanded" role="button">
+          {tabState === 1 ?
+            (
+              <>
+                {overviewData ? (
+                  <div className="ongoing-course-desc">
+                    <div className="ongoing-course-desc-title">
+                      <p>{overviewData.courseTagLine}</p>
+                    </div>
+                    <div className="ongoing-course-desc-content">
+                      <input type="checkbox" id="expanded"></input>
+                      <ShowMoreText
+                        className="showmore"
+                        anchorClass="show-more-style"
+                      >
+                        {overviewData.description}
+                      </ShowMoreText>
+                      {/* <ShowMoreText className="showmore" anchorClass="show-more-style">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis quod ullam, iste quo hic voluptatem, et tempore, modi quaerat placeat cum. Quam eos itaque quo omnis, quae delectus illo quos autem nobis ut possimus in, excepturi et illum sunt perspiciatis accusamus, repellat facilis fuga animi voluptatem pariatur nisi consequatur voluptates! Rem cum nobis itaque consequuntur eveniet a nihil ab quibusdam deleniti dolorum quia libero ullam culpa minima eaque debitis minus aperiam ducimus odio delectus, eius consequatur iusto odit! Nemo quasi corporis velit itaque neque fugit soluta dolores dolorem facere, molestias maxime non consequatur quidem odio totam esse iure, delectus fuga!</ShowMoreText> */}
+                      {/* <label for="expanded" role="button">
                     SHOW MORE
                   </label> */}
 
-                    {/* {
+                      {/* {
                       unmountStateRedux === 'true' ? <p>Unmount true</p> : <p>Unmount false</p>
                     } */}
+                    </div>
                   </div>
-                </div>
-              ) : (
-                <h3>Loading</h3>
-              )}
-            </>
-          ) : (
-            //  <div className="course-completion">
-            //     <div className="course-completion-section-1">
-            //         <div className="completion-section-1-main">
+                ) : (
+                  <h3>Loading</h3>
+                )}
+              </>
+            ) :
+            (
+              <>
+                {
+                  chapter && chapter.coursePercentage ?
+                    <div className="course-completion">
+                      <div className="course-completion-section-1-2">
+                        <div className="course-completion-section-1">
+                          <div className="completion-section-1-main">
 
-            //             <p className='completion-section-1-main-title'>Course Result</p>
-            //             <p className='completion-section-1-main-per'>90%</p>
-            //             <p className='completion-section-1-main-apr'>approval rate</p>
-            //         </div>
-            //     </div>
-            //     <div className="course-completion-section-2">
-            //         <div className="completion-section-2-main">
-            //             <div className="cmain-1">
-            //                 <p>Joined</p>
-            //                 <p className='cmain-1-date'>02/04/2021</p>
-            //             </div>
-            //             <div className="cmain-2">
-            //                 <p>Completed</p>
-            //                 <p className='cmain-1-date'>02/04/2021</p>
-            //             </div>
-            //             <div className="cmain-3">
-            //                 <p>Duration</p>
-            //                 <p className='cmain-1-date'>4h 30m</p>
-            //             </div>
-            //         </div>
-            //     </div>
-            //     <div className="course-completion-section-3">
-            //         <div className="completion-section-3-main">
-            //             <div className="cs3-main-1">
-            //                 <p className='cs3-main-1-title'>Course Certificate</p>
-            //                 <div className='download-icon-image'>{downloadIcon}</div>
-            //             </div>
-            //             <div className="cs3-main-2">
-            //                 <img src={require('../../../../assets/images/certicon.png')} alt="" />
-            //             </div>
-            //         </div>
-            //     </div>
-            // </div>
-            ''
-          )}
+                            <p className='completion-section-1-main-title'>Course Result</p>
+                            <p className='completion-section-1-main-per'>{chapter.coursePercentage.toFixed(0)}%</p>
+                            <p className='completion-section-1-main-apr'>approval rate</p>
+                          </div>
+                        </div>
+                        <div className="course-completion-section-2">
+                          <div className="completion-section-2-main">
+                            <div className="cmain-1">
+                              <p>Joined</p>
+                              <p className='cmain-1-date'>{chapter.joinedDate}</p>
+                            </div>
+                            <div className="cmain-2">
+                              <p>Completed</p>
+                              <p className='cmain-1-date'>{chapter.completedDate}</p>
+                            </div>
+                            <div className="cmain-3">
+                              <p>Duration</p>
+                              <p className='cmain-1-date'>{chapter.courseDuration}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="course-completion-section-3">
+                        <div className="completion-section-3-main">
+                          <div className="cs3-main-1">
+                            <p className='cs3-main-1-title'>Course Certificate</p>
+                            <div className='download-icon-image'>{downloadIcon}</div>
+                          </div>
+                          <div className="cs3-main-2">
+                            <img src={require('../../../../assets/images/certicon.png')} alt="" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    :
+                    ''
+                }
+              </>
+            )
+          }
         </div>
         <div className="ongoing-section-2">
           <div className="ongoing-container-1">
@@ -992,7 +996,7 @@ const OngoingOverview = () => {
                                 className="course-accordian"
                                 onClick={() => {
                                   accordianToggle(id);
-                                  dispatch(accordianIDState(id + 1));
+                                  // dispatch(accordianIDState(id + 1));
                                 }}
                               >
                                 <div className="course-accordian-heading">
@@ -1108,7 +1112,7 @@ const OngoingOverview = () => {
                                               statusTest
                                                 ? ele &&
                                                   ele.chapterTestPercentage &&
-                                                  ele.chapterTestPercentage > 0
+                                                  ele.chapterTestPercentage >= 0
                                                   ? attemptTest()
                                                   : dispatch(
                                                     test(
@@ -1137,7 +1141,7 @@ const OngoingOverview = () => {
                                               </div>
                                             </div>
                                             {ele.chapterTestPercentage &&
-                                              ele.chapterTestPercentage > 0 && (
+                                              ele.chapterTestPercentage >= 0 && (
                                                 <div className="percent-marks">
                                                   <div className="percent">
                                                     {ele.chapterTestPercentage.toFixed(
