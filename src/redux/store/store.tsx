8e5ef03d390a9bf70a653d755a2c0eb6f2bcea13
DeepthiNayak20 <deepthi.nayak@robosoftin.com>
2022-12-20 09:45:50 +0530

    initial commit

--- src/redux/store/store.tsx
+++ src/redux/store/store.tsx
@@ -1,35 +1,35 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
-import thunk from 'redux-thunk';
-import { showHeaderProfile } from '../reducers/headerProfileOptions';
-import showLoginConditions from '../reducers/Conditions';
-import loginSlice from '../reducers/loginSlice';
-import myCourseReducer from '../reducers/myCourseReducer';
-import { quizAnswerSlice } from '../reducers/result';
-import testSlice from '../reducers/testSlice';
-import allcourseReducer from '../reducers/allcourseSlice';
-import categoryReducer from '../reducers/categorySlice';
-import answerHeaderSlice from '../reducers/testAnswerHeader';
-import answerSlice from '../reducers/testAnswer';
-import { filterSlice } from '../reducers/filter';
-import { testSuccessRedSlice } from '../reducers/SuccessTestRed';
-import { showSuccessPageSlice } from '../reducers/showSuccesspage';
-import { finaltestShowPageSlice } from '../reducers/finalTestSuccess';
-import FinalResultSlice from '../reducers/finalResult';
-import NotifySlice from '../reducers/Notifications';
-import basicCourseSlice from '../reducers/basicCourses';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import thunk from 'redux-thunk'
+import { showHeaderProfile } from '../reducers/headerProfileOptions'
+import showLoginConditions from '../reducers/Conditions'
+import loginSlice from '../reducers/loginSlice'
+import myCourseReducer from '../reducers/myCourseReducer'
+import { quizAnswerSlice } from '../reducers/result'
+import testSlice from '../reducers/testSlice'
+import allcourseReducer from '../reducers/allcourseSlice'
+import categoryReducer from '../reducers/categorySlice'
+import answerHeaderSlice from '../reducers/testAnswerHeader'
+import answerSlice from '../reducers/testAnswer'
+import { filterSlice } from '../reducers/filter'
+import { testSuccessRedSlice } from '../reducers/SuccessTestRed'
+import { showSuccessPageSlice } from '../reducers/showSuccesspage'
+import { finaltestShowPageSlice } from '../reducers/finalTestSuccess'
+import FinalResultSlice from '../reducers/finalResult'
+import NotifySlice from '../reducers/Notifications'
+import basicCourseSlice from '../reducers/basicCourses'
 import advancedCourseSlice, {
   advancedCourse,
-} from './../reducers/advancedCourse';
-import subCategoriesSlice, { subCategories } from './../reducers/subCategories';
-import chapterResponseSlice from '../reducers/chapterResponses';
-import courseOverviewSlice from '../reducers/courseOverview';
-import { pauseTimeReducer } from '../reducers/pauseTime';
-import pauseSlice from '../reducers/pauseTimeSlice';
-import allCoursePWSlice from '../reducers/AllcoursePW';
-import { paginationSlice } from '../reducers/pagination';
+} from './../reducers/advancedCourse'
+import subCategoriesSlice, { subCategories } from './../reducers/subCategories'
+import chapterResponseSlice from '../reducers/chapterResponses'
+import courseOverviewSlice from '../reducers/courseOverview'
+import { pauseTimeReducer } from '../reducers/pauseTime'
+import pauseSlice from '../reducers/pauseTimeSlice'
+import allCoursePWSlice from '../reducers/AllcoursePW'
+import { paginationSlice } from '../reducers/pagination'
 
 const reducers = combineReducers({
   headerProfile: showHeaderProfile.reducer,
@@ -56,23 +56,23 @@ const reducers = combineReducers({
   pauseUnmount: pauseSlice.reducer,
   allCoursePW: allCoursePWSlice.reducer,
   pagination: paginationSlice.reducer,
-});
+})
 
 const persistConfig = {
   key: 'root',
   storage,
   blacklist: ['login', 'pagination'],
-};
+}
 
-const persistedReducer = persistReducer(persistConfig, reducers);
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
   reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
   middleware: [thunk],
-});
+})
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
