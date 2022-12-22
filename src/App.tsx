9231964d383813b4ec5827f3f214eabbaa52b497
MamathaRao02 <mamatha.rao@robosoftin.com>
2022-12-22 12:07:30 +0530

    Changed in notification By Mamatha

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import PasswordChanged from './views/onboarding/password-changed/PasswordChanged
 import Success_Page from './views/onboarding/success_page/Success_Page';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ScrollToTop from "react-scroll-to-top";
-import './messaging_init_in_sw'
+import './messaging_init_in_sw';
 const theme = createTheme({
   palette: {
     primary: {
